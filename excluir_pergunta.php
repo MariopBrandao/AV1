4bf6c91cbd1a58d12c227a75fabd590b3aff1592
Mario Brandao <127356019+MariopBrandao@users.noreply.github.com>
2023-05-16 12:18:30 -0300

    Add files via upload

--- excluir_pergunta.php
+++ excluir_pergunta.php
@@ -1,32 +1,46 @@
 <?php
-function excluirPergunta($nomeArquivo) {
-    $arquivo = "{$nomeArquivo}.txt";
-    if (file_exists($arquivo)) {
-        unlink($arquivo);
-        echo "Pergunta excluída com sucesso.";
-    } else {
-        echo "Pergunta não encontrada.";
+function listarPerguntas() {
+    $arquivos = glob("*.txt");
+    if (empty($arquivos)) {
+        echo "Não há perguntas cadastradas.";
+        return;
+    }
+
+    echo "Perguntas existentes:<br>";
+    foreach ($arquivos as $arquivo) {
+        $conteudo = file_get_contents($arquivo);
+        $pergunta = preg_match("/Pergunta: (.+)/", $conteudo, $matches) ? $matches[1] : "Pergunta desconhecida";
+        echo "- $pergunta<br>";
     }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nomeArquivo = $_POST['arquivo'];
 
-    excluirPergunta($nomeArquivo);
+    if (!file_exists("$nomeArquivo.txt")) {
+        echo "Pergunta não encontrada.";
+        exit;
+    }
+
+    unlink("$nomeArquivo.txt");
+    echo "Pergunta excluída com sucesso.";
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Excluir uma Pergunta</title>
+    <title>Excluir Pergunta</title>
 </head>
 <body>
-    <h2>Excluir uma Pergunta</h2>
+    <h2>Selecionar Pergunta para Excluir</h2>
+    <?php listarPerguntas(); ?>
+    <br>
+    <h2>Selecione a Pergunta a Ser Excluída</h2>
     <form action="excluir_pergunta.php" method="POST">
         <label>Nome do Arquivo:</label><br>
         <input type="text" name="arquivo"><br><br>
-        <input type="submit" value="Excluir Pergunta">
+        <input type="submit" name="excluir" value="Excluir Pergunta">
     </form>
 </body>
 </html>
